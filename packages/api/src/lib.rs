91e0df040dccbed52bd1bf369a6ed269decd70c4
Tawansongsang Karnkawinpong <tawansongsang.k@ipthailand.go.th>
2025-12-04 17:17:44 +0700

    work 20251204

--- packages/api/src/lib.rs
+++ packages/api/src/lib.rs
@@ -1,8 +1,30 @@
 //! This crate contains all shared fullstack server functions.
+
+use dioxus::logger::tracing;
 use dioxus::prelude::*;
+
+#[cfg(feature = "server")]
+mod surreal_db {
+    use db::surrealdb::{self, DB};
+
+    pub async fn test() -> String {
+        surrealdb::connection_db().await.unwrap();
+        let surql = "RETURN 'OK'";
+        let tmp = DB.query(surql).await.unwrap().check().unwrap();
+        format!("{:?}", tmp)
+    }
+}
 
 /// Echo the user input on the server.
 #[post("/api/echo")]
 pub async fn echo(input: String) -> Result<String, ServerFnError> {
     Ok(input)
+}
+
+#[get("/api/test_connection")]
+pub async fn test_connection() -> Result<String, ServerFnError> {
+    let surql = "RETURN 'OK'";
+    let tmp = surreal_db::test().await;
+    tracing::info!("{}", tmp);
+    Ok("Success".to_string())
 }
